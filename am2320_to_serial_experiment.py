f62f01dfae8f46f0854cafd40514aeb4c41143a8
Peter Robinson <pjr@itee.uq.edu.au>
2021-03-07 12:26:07 +1000

    Update am2320_to_serial_experiment.py

--- am2320_to_serial_experiment.py
+++ am2320_to_serial_experiment.py
@@ -1,3 +1,13 @@
+# The Pico side of the serial connection for
+# accessing an am2320 temperature/humidity sensor
+#
+# When this side reads the string ';;get_temp;;'
+# it responds by using I2C comms to access the am2320
+# and sends the data over the serial line.
+#
+# Note the comments in the README about this sort of serial
+# communication
+
 import board
 import digitalio
 
@@ -11,10 +21,7 @@ sensor = adafruit_am2320.AM2320(i2c)
 
 while True:
     cmd = input().strip()
-    print(cmd)
-    if cmd == 'get':
-        print('data({{temperature:{0}, relative_humidity:{1}}})'.\
+    if cmd == ';;get_temp;;':
+        print('data({{"temperature":{0}, "relative_humidity":{1}}})'.\
               format(sensor.temperature, 
                      sensor.relative_humidity))
-    else:
-        break
