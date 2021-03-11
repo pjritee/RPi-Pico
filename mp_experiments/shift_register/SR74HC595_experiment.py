b86f5fe923f8b7b2eb0e437e08d6b11a9cd3fb28
pjr <pjr@itee.uq.edu.au>
2021-03-11 11:25:28 +1000

    change button to use IRQ and debouncing

--- mp_experiments/shift_register/SR74HC595_experiment.py
+++ mp_experiments/shift_register/SR74HC595_experiment.py
@@ -1,10 +1,52 @@
 
+# This experiment is like cp_experiments/shift_register/SR74HC595_experiment3.py
+# except that, instead of inheriting from the SR74HC595 class I define a class
+# similar to SR74HC59 defined in sr74HC595.py but only uses on_pins to set the pins that are on.
+
+# The other difference is that I have defined a DebouncedLightsButton class that
+# uses an IRQ callback on the button with debouncing
+
+
 import machine
 import time
 import sr74HC595
 
+# This class keeps track of the number of lights that should be on using an IRQ callback
+# and debouncing
+class DebouncedLightsButton(machine.Pin):
 
-button = machine.Pin(13, machine.Pin.IN, machine.Pin.PULL_DOWN)
+    # number of retries of the buttons value to determine if the value is stable
+    RETRIES = 40
+    
+    def __init__(self, pinnum, pinupdown, irq):
+        super().__init__(pinnum, machine.Pin.IN, pinupdown)
+        # setup an IRQ triggered callback for the button
+        self.irq(trigger=irq, handler=self.callback)
+        self._num_lights = 1
+        self.required_value = 0 if irq == machine.Pin.IRQ_FALLING else 1
+
+    def debounce(self):
+        # return True iff the button is stable at 1 over RETRIES checks
+        curr = self.value()
+        if curr != self.required_value:
+            return False
+        for _ in range(self.RETRIES):
+            next_ = self.value()
+            if curr != next_:
+                return False
+            curr = next_
+        return True
+    
+    def callback(self, _):
+        # callback for button
+        if self.debounce():
+            self._num_lights = self._num_lights % 7 + 1
+    
+    @property
+    def num_lights(self):
+        return self._num_lights
+    
+button = DebouncedLightsButton(13, machine.Pin.PULL_DOWN, machine.Pin.IRQ_RISING)
 
 latch_pin =  machine.Pin(6, machine.Pin.OUT)
 
@@ -17,20 +59,11 @@ spi = machine.SPI(0, baudrate=5000000, polarity=0, phase=0,
 sr = sr74HC595.SR74HC595(spi, latch_pin)
 
 def main():
-    num_lights = 1
-    button_pressed = False
     
     while True:
         for index in range(8):
-            # update number of lights if button is pressed
-            if button.value() and not button_pressed:
-                button_pressed = True
-                num_lights = num_lights % 7 + 1
-            if button.value() == 0 and button_pressed:
-                button_pressed = False
-    
             sr.on_pins = (index+offset \
-                          for offset in range(num_lights))
+                          for offset in range(button.num_lights))
         
             delay = potentiometer.read_u16()/200000
             time.sleep(delay)
